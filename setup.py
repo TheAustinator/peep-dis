cd0f06b8a96e0a7d89473899f049c895b4caad3c
theaustinator <austinthepostin@gmail.com>
2019-05-25 14:41:48 -0700

    versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peepdis',
-    version='0.1.5',
+    version='0.1.0',
     description='Terminal object inspector for python',
     author='Austin McKay',
     author_email='austinmckay303@gmail.com',
