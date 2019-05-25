d362e65196d6ec34c02076a34e7a1e1383d2e2cd
theaustinator <austinthepostin@gmail.com>
2019-05-25 15:02:12 -0700

    PyPI and CI versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peepdis',
-    version='0.1.7',
+    version='0.1.8',
     description='Terminal object inspector for python',
     author='Austin McKay',
     author_email='austinmckay303@gmail.com',
